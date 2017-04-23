f6af944379b184b6224d5cb8578e8ceee9805915
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-04-24 01:25:30 +0300

    Add plugin main entry point class

---
+++ Plugin.BingSpeech/BingSpeech.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Plugin.BingSpeech.Abstractions.Interfaces;
+using Plugin.BingSpeech.Services;
+
+namespace Plugin.BingSpeech
+{
+    public sealed class BingSpeech
+    {
+        private static Lazy<BingSpeech> bingSpeech 
+            = new Lazy<BingSpeech>(() => new BingSpeech(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        public readonly IBingSpeechService BingSpeechService = null;
+        public readonly IMicrophoneService MicrophoneService = null;
+
+        private BingSpeech()
+        {
+#if !PORTABLE
+            this.MicrophoneService = new MicrophoneService();
+#endif
+            this.BingSpeechService = new BingSpeechService();
+        }
+
+        public static BingSpeech Current
+        {
+            get
+            {
+                var service = bingSpeech.Value;
+
+                if (service == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                return service;
+            }
+        }
+
+        public void InitializePlugin(string subscriptionKey)
+        {
+            var authenticationService = new AuthenticationService(subscriptionKey);
+
+            this.BingSpeechService.InitializeService(authenticationService);
+        }
+    }
+}
