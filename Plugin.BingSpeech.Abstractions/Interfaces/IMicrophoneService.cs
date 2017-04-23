1536fb044cd212e22ad7b04ac7397a069c0480f5
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-04-24 01:24:14 +0300

    Add microphone interface to use in platform specific code

---
+++ Plugin.BingSpeech.Abstractions/Interfaces/IMicrophoneService.cs
@@ -0,0 +1,13 @@
+﻿namespace Plugin.BingSpeech.Abstractions.Interfaces
+{
+    public interface IMicrophoneService
+    {
+        void StartRecording();
+
+        void StopRecording();
+
+        void ContinuousDictation();
+
+        void RemoveRecording();
+    }
+}
