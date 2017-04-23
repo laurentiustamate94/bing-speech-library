1584ead3a07f0df76eef9b1092a9b06a9ead5a87
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-04-24 01:24:38 +0300

    Add exception to be thrown in platform specific code

---
+++ Plugin.BingSpeech.Abstractions/Exceptions/MicrophoneServiceException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Plugin.BingSpeech.Exceptions
+{
+    public sealed class MicrophoneServiceException : Exception
+    {
+        public MicrophoneServiceException()
+        {
+        }
+
+        public MicrophoneServiceException(string message)
+            : base(message)
+        {
+        }
+
+        public MicrophoneServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
